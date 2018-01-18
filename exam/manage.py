e6a0aac9d25d6f90437a998993e26015f56ffff3
Dmitriy Otstavnov <tabroot@yandex-team.ru>
2018-01-18 21:17:57 +0300

    fixes

--- exam/manage.py
+++ exam/manage.py
@@ -3,7 +3,13 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "exam.settings")
+    RUNNING_DEVSERVER = (len(sys.argv) > 1 and sys.argv[1] == 'runserver')
+
+    if RUNNING_DEVSERVER:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "exam.settings")
+    else :
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "exam.settings_test")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
